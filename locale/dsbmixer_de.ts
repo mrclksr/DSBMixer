102f4a9f14255e78a423a7efac175ddfdbc27fe4
Marcel <mk@freeshell.de>
2017-07-21 01:16:33 +0200

    Update translation

--- locale/dsbmixer_de.ts
+++ locale/dsbmixer_de.ts
@@ -4,14 +4,14 @@
 <context>
     <name>ChanSlider</name>
     <message>
-        <location filename="../src/chanslider.cpp" line="50"/>
-        <location filename="../src/chanslider.cpp" line="106"/>
+        <location filename="../src/chanslider.cpp" line="49"/>
+        <location filename="../src/chanslider.cpp" line="104"/>
         <source>Set/unset recording source</source>
         <translation>Aufnahmequelle aktivieren/deaktieren</translation>
     </message>
     <message>
-        <location filename="../src/chanslider.cpp" line="72"/>
-        <location filename="../src/chanslider.cpp" line="138"/>
+        <location filename="../src/chanslider.cpp" line="71"/>
+        <location filename="../src/chanslider.cpp" line="136"/>
         <source>Mute</source>
         <translation>Stumm</translation>
     </message>
@@ -19,17 +19,17 @@
 <context>
     <name>MainWin</name>
     <message>
-        <location filename="../src/mainwin.cpp" line="242"/>
+        <location filename="../src/mainwin.cpp" line="260"/>
         <source>&amp;Quit</source>
         <translation>&amp;Beenden</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="243"/>
+        <location filename="../src/mainwin.cpp" line="261"/>
         <source>&amp;Preferences</source>
         <translation>&amp;Einstellungen</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="253"/>
+        <location filename="../src/mainwin.cpp" line="271"/>
         <source>&amp;File</source>
         <translation>&amp;Datei</translation>
     </message>
@@ -37,75 +37,80 @@
 <context>
     <name>Preferences</name>
     <message>
-        <location filename="../src/preferences.cpp" line="53"/>
+        <location filename="../src/preferences.cpp" line="54"/>
         <source>&amp;Apply</source>
         <translation>&amp;Anwenden</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="57"/>
+        <location filename="../src/preferences.cpp" line="58"/>
         <source>&amp;Cancel</source>
         <translation>&amp;Abbrechen</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="60"/>
+        <location filename="../src/preferences.cpp" line="61"/>
         <source>View</source>
         <translation>Ansicht</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="61"/>
+        <location filename="../src/preferences.cpp" line="62"/>
         <source>Default device</source>
         <translation>Standardgerät</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="62"/>
+        <location filename="../src/preferences.cpp" line="63"/>
         <source>Advanced</source>
         <translation>Erweitert</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="72"/>
+        <location filename="../src/preferences.cpp" line="73"/>
         <source>Preferences</source>
         <translation>Einstellungen</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="121"/>
+        <location filename="../src/preferences.cpp" line="123"/>
         <source>Select mixer devices to be visible
 </source>
         <translation>Auswahl anzuzeigender Geräte</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="130"/>
+        <location filename="../src/preferences.cpp" line="132"/>
         <source>Show left and right channel</source>
         <translation>Zeige linken und rechten Kanal</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="144"/>
+        <location filename="../src/preferences.cpp" line="135"/>
+        <source>Show ticks</source>
+        <translation>Zeige Skalenmarkierungen</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="151"/>
         <source>Select default sound device
 </source>
         <translation>Auswahl des Standard-Audiogeräts</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="169"/>
+        <location filename="../src/preferences.cpp" line="176"/>
         <source>Advanced settings
 </source>
         <translation>Erweiterte Einstellungen</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="188"/>
+        <location filename="../src/preferences.cpp" line="195"/>
         <source>Amplification:</source>
         <translation>Verstärkung:</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="194"/>
+        <location filename="../src/preferences.cpp" line="201"/>
         <source>Feeder rate quality:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="198"/>
+        <location filename="../src/preferences.cpp" line="205"/>
         <source>Vchans for playback (pcm%1):</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="203"/>
+        <location filename="../src/preferences.cpp" line="210"/>
         <source>Vchans for recording:</source>
         <translation type="unfinished"></translation>
     </message>
