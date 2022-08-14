f5b462b8b37f665a7b7973e83f92d6e77373220b
bittin <bittin@reimu.nl>
2022-08-14 16:16:08 +0200

    Added Swedish Translation
    Added Swedish Translation :)

---
+++ locale/dsbmixer_sv.ts
@@ -0,0 +1,221 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv_SE">
+<context>
+    <name>ChanSlider</name>
+    <message>
+        <location filename="../src/chanslider.cpp" line="62"/>
+        <location filename="../src/chanslider.cpp" line="154"/>
+        <source>mute</source>
+        <translation>tysta</translation>
+    </message>
+    <message>
+        <location filename="../src/chanslider.cpp" line="100"/>
+        <location filename="../src/chanslider.cpp" line="181"/>
+        <source>Set/unset recording source</source>
+        <translation>aktivera/avaktivera inspelningskällan</translation>
+    </message>
+</context>
+<context>
+    <name>MainWin</name>
+    <message>
+        <location filename="../src/mainwin.cpp" line="426"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Avsluta</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="427"/>
+        <source>&amp;Preferences</source>
+        <translation>&amp;Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="428"/>
+        <source>Show/hide window</source>
+        <translation>Visa/dölj fönster</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="439"/>
+        <source>&amp;File</source>
+        <translation>&amp;Fil</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="507"/>
+        <location filename="../src/mainwin.cpp" line="544"/>
+        <source>Muted</source>
+        <translation>Tystad</translation>
+    </message>
+</context>
+<context>
+    <name>Preferences</name>
+    <message>
+        <location filename="../src/preferences.cpp" line="67"/>
+        <source>&amp;Ok</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="71"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Avbryt</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="74"/>
+        <source>View</source>
+        <translation>Visa</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="75"/>
+        <source>Default device</source>
+        <translation>Standardenhet</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="76"/>
+        <source>Advanced</source>
+        <translation>Avancerat</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="89"/>
+        <source>Preferences</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="175"/>
+        <source>Select mixer devices to be visible
+</source>
+        <translation>Välj mixer enheter som ska vara synliga</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="176"/>
+        <source>Slider settings</source>
+        <translation>Reglageinställning</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="177"/>
+        <source>Tray icon theme</source>
+        <translation>Tray ikon tema</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="197"/>
+        <source>Show left and right channel</source>
+        <translation>Visa höger och vänster kanal</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="200"/>
+        <source>Show ticks</source>
+        <translation>Visa ticks</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="225"/>
+        <source>Select default sound device</source>
+        <translation>Välj standard ljudenhet</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="226"/>
+        <location filename="../src/preferences.cpp" line="308"/>
+        <source>Test sound</source>
+        <translation>Testa ljud</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="243"/>
+        <source>Enter a command which plays a sound</source>
+        <translation>Ange ett kommando som spelar ett ljud</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="295"/>
+        <source>Stop</source>
+        <translation>Stoppa</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="337"/>
+        <source>Advanced settings</source>
+        <translation>Avancerade inställningar</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="344"/>
+        <source>Bypass mixer</source>
+        <translation>Gå förbi Mixer</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="346"/>
+        <source>Enable this to allow applications to use their own existing mixer logic to control their own channel volume.</source>
+        <translation>Aktivera detta för att tillåta program att använda sin egen befintliga mixerlogik för att styra sin egna kanalvolym.</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="352"/>
+        <source>Defines the max. number of virtual playback and recording channels that can be created. Virtual channels allow programs to use more playback and recording channels than the physical hardware provides.</source>
+        <translation>Definierar max. antal virtuella uppspelnings och inspelningskanaler som kan skapas. Virtuella kanaler tillåter program att använda fler uppspelnings och inspelningskanaler än vad den fysiska hårdvaran ger.</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="361"/>
+        <source>Lower values mean less buffering and latency.</source>
+        <translation>Lägre värde innebär mindre buffring och latens.</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="366"/>
+        <source>Lower values mean more amplification, but can produce sound clipping when chosen too low. Higher values mean finer volume control.
+        <translation>Lägre värden betyder mer förstärkning, men kan producera ljudklippning när det väljs för lågt. Högre värden innebär finare volymkontroll.</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="372"/>
+        <source>Higher values mean better sample rate conversion, but more memory and CPU usage.</source>
+        <translation>Högre värden betyder bättre samplingsfrekvensomvandling, men mer minne och CPU-användning</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="379"/>
+        <source>Defines the time interval in milliseconds mixer devices are polled. Higher values mean less CPU usage. Lower values mean less latency when showing the current volume changed by other programs.</source>
+        <translation>Definierar tidsintervallet i millisekunder som mixerenheter pollas. Högre värden betyder mindre CPU-användning. Lägre värden innebär mindre latens när den aktuella volymen visas som ändrats av andra program.</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="387"/>
+        <source>Amplification:</source>
+        <translation>Förstärkning:</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="391"/>
+        <source>Sample rate converter quality:</source>
+        <translation>Sample rate konverter kvalitet:</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="395"/>
+        <source>Max. auto VCHANS:</source>
+        <translation>Max. automatiska VCHANS:</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="399"/>
+        <source>Latency (0 low, 10 high):</source>
+        <translation>Latens (0 låg, 10 hög):</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="403"/>
+        <source>Poll mixers every</source>
+        <translation>Avfråga mixer var</translation>
+    </message>
+</context>
+<context>
+    <name>RestartApps</name>
+    <message>
+        <location filename="../src/restartapps.cpp" line="40"/>
+        <source>&lt;b&gt;Default audio device changed&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Standard-Ljud enhet ändrad&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/restartapps.cpp" line="41"/>
+        <source>Restart audio applications:</source>
+        <translation>Starta om ljud program:</translation>
+    </message>
+    <message>
+        <location filename="../src/restartapps.cpp" line="45"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Avbryt</translation>
+    </message>
+    <message>
+        <location filename="../src/restartapps.cpp" line="46"/>
+        <source>&amp;Restart</source>
+        <translation>&amp;Starta om</translation>
+    </message>
+    <message>
+        <location filename="../src/restartapps.cpp" line="51"/>
+        <source>DSBMixer - Restart audio applications</source>
+        <translation>DSBMixer - Starta om ljud program</translation>
+    </message>
+</context>
+</TS>
